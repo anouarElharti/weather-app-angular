e7eb2a4286cffe6d38694c4e884eb095490be79d
Anouar Elharti <ElhartAn@basfad.basf.net>
2022-06-16 10:34:06 +0100

    Adding search form to get the weather data for other cities

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,10 +10,19 @@ import { WeatherData } from './models/weather.model';
 export class AppComponent implements OnInit {
   constructor(private weatherService: WeatherService) {}
 
+  cityName: string = 'Rabat';
   weatherData?: WeatherData;
 
   ngOnInit(): void {
-    this.weatherService.getWeatherData('Casablanca').subscribe({
+    this.getWeatherData(this.cityName);
+    this.cityName = '';
+  }
+  onSubmit() {
+    this.getWeatherData(this.cityName);
+    this.cityName = '';
+  }
+  private getWeatherData(cityName: string) {
+    this.weatherService.getWeatherData(cityName).subscribe({
       next: (response) => {
         this.weatherData = response;
         console.log('Weather data', response);
